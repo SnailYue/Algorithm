50e4e373f8f6d2e97df1bd74ba927c8dca469034
Snai <yuesnail@gmail.com>
2019-11-26 22:04:09 +0800

    优化买卖股票最佳时期问题

--- src/com/snail/dynamicplanning/BestTimeTobuyAndSellStocks.java
+++ src/com/snail/dynamicplanning/BestTimeTobuyAndSellStocks.java
@@ -12,38 +12,24 @@ public class BestTimeTobuyAndSellStocks {
      * 只需要求出后面的一天的价格减去前一天的价格得到最大的值即可。
      * 这样将一个大的问题分成若干个小的问题。
      *
-     * @param arr
+     * @param prices
      * @return
      */
-    public static int findMaxSumOfArrays(int[] arr) {
-        if (null == arr || 0 == arr.length) {
+    public static int findMaxSumOfArrays(int[] prices) {
+        if (null == prices || 0 == prices.length) {
             return 0;
         }
-        int minStart = arr[0];
-        int maxEnd = arr[1];
-        for (int i = 1; i < arr.length; i++) {
-            int result = arr[i] - minStart;
-            /**
-             * 当相减的结果大于之前的最大的结果时，说明当前的利润更大，记录当前的值
-             */
-            if (result > (maxEnd - minStart)) {
-                maxEnd = arr[i];
-            }
-            /**
-             * 当相减的结果小于之前的最大结果时，再让其与当前最小的值相比较，如果小于当前的最小值。
-             * 则将当前的值设为最小值。
-             */
-            else {
-                if (minStart > arr[i]) {
-                    minStart = arr[i];
-                }
-            }
+        int max = 0;
+        int min = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            min = prices[i] >= min ? min : prices[i];
+            max = max >= (prices[i] - min) ? max : (prices[i] - min);
         }
-        return maxEnd - minStart;
+        return max;
     }
 
     public static void testInstance() {
-        int[] arrays = new int[]{7, 2, 5, 1, 6, 4};
+        int[] arrays = new int[]{7, 1, 5, 3, 6 , 4};
         int result = findMaxSumOfArrays(arrays);
         System.out.println("result = " + result);
     }
